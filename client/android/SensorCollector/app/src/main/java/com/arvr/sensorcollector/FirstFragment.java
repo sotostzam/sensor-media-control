5f1581699f10b80ffd2a903a257071565098ce4f
Archit Yadav <archityadav97@gmail.com>
2021-11-20 21:40:55 +0100

    touch-based colour change for circular button, correct text-field for IP address and minor change for connect button

--- client/android/SensorCollector/app/src/main/java/com/arvr/sensorcollector/FirstFragment.java
+++ client/android/SensorCollector/app/src/main/java/com/arvr/sensorcollector/FirstFragment.java
@@ -1,19 +1,26 @@
 package com.arvr.sensorcollector;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FirstFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment
+{
+
+    private ImageButton mImageButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,9 +63,36 @@ public class FirstFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+
+        mImageButton = (ImageButton) view.findViewById(R.id.imageButton);
+
+        // Changing colour of image during runtime taken from:
+        // https://stackoverflow.com/a/35286182/6475377
+        mImageButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    mImageButton.setColorFilter(getResources().getColor(android.R.color.holo_red_dark), PorterDuff.Mode.SRC_ATOP);
+                } else if (event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    mImageButton.setColorFilter(getResources().getColor(android.R.color.holo_blue_light), PorterDuff.Mode.SRC_ATOP);
+                }
+                return false;
+            }
+
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
+
+//    public class CircleButtonListener implements
 }
