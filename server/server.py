795a901b309c1686611127d8e7af019690bf1870
Sotiris Tzamaras <sotos@tzamaras.com>
2021-12-10 10:03:42 +0100

    organize server app and implement initial interaction frame

--- server/server.py
+++ server/server.py
@@ -12,7 +12,12 @@ class Server:
     Main server class. It initializes the variables needed to display the graphical user interface
     and opens a UDP socket which listens to a user-defined port.
     """
+
     def __init__(self):
+        self.create_gui()
+        self.create_udp_stream()
+
+    def create_gui(self):
 
         self.host = ''
         self.port = 50000
@@ -21,25 +26,27 @@ class Server:
 
         # Get default audio device using PyCAW
         self.devices = AudioUtilities.GetSpeakers()
-        self.interface = self.devices.Activate(
-            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
         self.volume = cast(self.interface, POINTER(IAudioEndpointVolume))
 
         # Window and canvas initialization parameters
         self.width  = 800
         self.height = 600
-        self.w = self.width / 3
-        self.h = self.height / 3
         self.window = tk.Tk()
         self.window.title("Android's Sensors")
 
+        self.create_sensor_frame()
+        self.create_tabs_frame()
+
+    def create_sensor_frame(self):
+        
         # Main sensor Frame
         self.sensor_frame = tk.Frame(self.window)
         self.sensor_frame.grid(row=0, column=0, sticky="nswe")
         self.sensor_frame.rowconfigure(0, minsize=100, weight=0)
 
         # Gyroscope Data
-        self.gyro_data_frame = tk.Frame(self.sensor_frame, bg = "black")
+        self.gyro_data_frame = tk.Frame(self.sensor_frame)
         self.gyro_data_frame.grid(row=0, column=0, sticky="we", padx=0)
 
         self.gyro_data_x_label = tk.Label(self.gyro_data_frame, text="Gyroscope x:")
@@ -63,9 +70,8 @@ class Server:
         self.label_gyro_z = tk.Label(self.gyro_data_frame, textvariable=self.gyro_z)
         self.label_gyro_z.grid(row=2, column=1, sticky="we", padx=25)
 
-
         # Positional Data
-        self.position_data_frame = tk.Frame(self.sensor_frame, bg = "black")
+        self.position_data_frame = tk.Frame(self.sensor_frame)
         self.position_data_frame.grid(row=1, column=0, sticky="we", padx=0)
 
         self.position_data_x_label = tk.Label(self.position_data_frame, text="Acceleration x:")
@@ -89,7 +95,7 @@ class Server:
         self.label_position_z = tk.Label(self.position_data_frame, textvariable=self.position_z)
         self.label_position_z.grid(row=2, column=1, sticky="we", padx=25)
 
-
+    def create_tabs_frame(self):
         # Right Frame
         self.right_frame = tk.Frame(self.window, width = self.width, height = self.height, bg="lightblue")
         self.right_frame.grid(row=0, column=1, sticky="nswe")
@@ -98,13 +104,79 @@ class Server:
         self.notebook = ttk.Notebook(self.right_frame)
         self.notebook.grid(row=0, column=0, sticky="nswe")
 
-        self.frame1 = tk.Frame(self.notebook, bg="green")
-        self.frame2 = tk.Frame(self.notebook, bg="red")
+        self.frame1 = tk.Frame(self.notebook, width=self.width, height=self.height)
+        self.create_settings(self.frame1)
+
+        self.canvas_interaction = tk.Canvas(master = self.window, width = self.width, height = self.height, bg="grey")
+        self.create_interaction_frame(self.canvas_interaction)
 
         self.notebook.add(self.frame1, text='Settings')
-        self.notebook.add(self.frame2, text='Tests')
+        self.notebook.add(self.canvas_interaction, text='Interaction Testing')
 
-        self.create_udp_stream()
+    def create_settings(self, parent):
+
+        def ok(*args):
+            print ("value is:" + self.variable.get())
+
+        OPTIONS = [
+        "Left Gesture",
+        "Right Gesture",
+        "Up Gesture",
+        "Down Gesture"
+        ]
+
+        gestures = tk.Label(parent, text="GESTURES")
+        gestures.grid(row=0, column=0, sticky="we")
+
+        self.testlabel = tk.Label(parent, text="Play/Pause: ")
+        self.testlabel.grid(row=1, column=0, sticky="we")
+
+        self.variable = tk.StringVar(parent)
+        self.variable.set(OPTIONS[2]) # default value
+
+        self.w = ttk.OptionMenu(parent, self.variable, OPTIONS[2], *OPTIONS, command=ok)
+        self.w.grid(row=1, column=1, sticky="we")
+
+        self.testlabel1 = tk.Label(parent, text="Previous: ")
+        self.testlabel1.grid(row=2, column=0, sticky="we")
+
+        self.variable1 = tk.StringVar(parent)
+        self.variable1.set(OPTIONS[0]) # default value
+
+        self.w1 = ttk.OptionMenu(parent, self.variable1, OPTIONS[0], *OPTIONS, command=ok)
+        self.w1.grid(row=2, column=1, sticky="we")
+
+    def create_interaction_frame(self, parent):
+
+        def test():
+            print("It works!")
+
+        self.photo_play  = tk.PhotoImage(file = r"./server/sources/play.png").subsample(5,5)
+        self.photo_pause = tk.PhotoImage(file = r"./server/sources/pause.png").subsample(5,5)
+        self.photo_next  = tk.PhotoImage(file = r"./server/sources/next.png").subsample(5,5)
+        self.photo_prev  = tk.PhotoImage(file = r"./server/sources/previous.png").subsample(5,5)
+        self.photo_stop  = tk.PhotoImage(file = r"./server/sources/stop.png").subsample(5,5)
+
+        self.interaction_button = ttk.Button(parent, text= "Play", image=self.photo_play, compound=tk.LEFT, command= test)
+        self.interaction_button.grid(row=0, column=0, sticky="we")
+
+        self.interaction_button_pause = ttk.Button(parent, text= "Pause", image=self.photo_pause, compound=tk.LEFT, command= test)
+        self.interaction_button_pause.grid(row=0, column=3, sticky="we")
+
+        self.interaction_button_next = ttk.Button(parent, text= "Next", image=self.photo_next, compound=tk.LEFT, command= test)
+        self.interaction_button_next.grid(row=1, column=3, sticky="we")
+
+        self.interaction_button_prev = ttk.Button(parent, text= "Previous", image=self.photo_prev, compound=tk.LEFT, command= test)
+        self.interaction_button_prev.grid(row=1, column=0, sticky="we")
+        
+        self.interaction_button_stop = ttk.Button(parent, text= "Stop", image=self.photo_stop, compound=tk.LEFT, command= test)
+        self.interaction_button_stop.grid(row=2, column=1, sticky="we")
+
+        self.interaction_volume = ttk.Scale(parent, from_=0, to=100, orient="vertical")
+        self.interaction_volume.grid(row=3, column=1, sticky="w")
+
+        self.interaction_seek = ttk.Scale(parent, from_=0, to=100)
+        self.interaction_seek.grid(row=4, column=1, sticky="we")
 
     def set_udp(self, host, port):
         """
